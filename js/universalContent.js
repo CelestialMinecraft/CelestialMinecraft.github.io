bd97f8809dd8fc6b761819993f289d399e83d304
AtomicKeith76 <atomickeith76@gmail.com>
2026-08-05 00:03:09 -0500

    Pages :P

--- js/universalContent.js
+++ js/universalContent.js
@@ -1,7 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("headerMain").innerHTML = "<h1>Celestial MC</h1>";
     document.getElementById("navMain").innerHTML = `
-        <ul class=\"navMain\"><li><a href=\"#\">Home</a></li><li><a href=\"#\">Page 2</a></li><li><a href=\"#\">Page 3</a></li></ul>
+        <ul class=\"navMain\">
+            <li><a href=\"./index.html">Home</a></li>
+            <li><a href=\"./plugins.html">Plugins</a></li>
+        </ul>
     `;
     document.getElementById("footerMain").innerHTML = "<p>© 2026 Celestial MC. All rights reserved.</p>";
 });
