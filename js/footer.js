31e6aabf802febd855008cfd0993a710a8fcc03f
AtomicKeith76 <atomickeith76@gmail.com>
2026-08-04 23:58:05 -0500

    Saving Progress

--- js/footer.js
+++
@@ -1,3 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("footerMain").innerHTML = "<p>© 2026 Celestial MC. All rights reserved.</p>";
-});
---
+++ js/universalContent.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("headerMain").innerHTML = "<h1>Celestial MC</h1>";
+    document.getElementById("navMain").innerHTML = `
+        <ul class=\"navMain\"><li><a href=\"#\">Home</a></li><li><a href=\"#\">Page 2</a></li><li><a href=\"#\">Page 3</a></li></ul>
+    `;
+    document.getElementById("footerMain").innerHTML = "<p>© 2026 Celestial MC. All rights reserved.</p>";
+});
